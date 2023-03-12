5439aaf9a47c3ab94b70068788eaafab6864c3b4
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-03-12 22:44:43 +0200

    fix search params

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,59 +1,53 @@
-import { notFound } from "next/navigation";
+"use client";
+import { notFound, useSearchParams } from "next/navigation";
 import defaultImg from "../../public/defaultImg.jpeg";
 import Image from "next/image";
 
-type Props = {
-  searchParams?: DataEntry;
-};
+function ArticlePage() {
+  const router = useSearchParams();
+  let searchParams!: DataEntry;
+  for (const [key, value] of router.entries()) {
+    searchParams = { ...searchParams, [key]: value };
+  }
 
-function ArticlePage({ searchParams }: Props) {
-  console.log(searchParams);
-  //   if (
-  //     (searchParams && Object.entries(searchParams).length) === 0 ||
-  //     !searchParams
-  //   ) {
-  //     return notFound();
-  //   }
+  if (
+    (searchParams && Object.entries(searchParams).length) === 0 ||
+    !searchParams
+  ) {
+    return notFound();
+  }
 
-  //   const { description, image, title, published_at, source, author }: DataEntry =
-  //     searchParams;
-  //   return (
-  //     <section>
-  //       <article className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
-  //         {!image ? (
-  //           <Image
-  //             className="max-w-xs mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
-  //             alt="image"
-  //             src={defaultImg}
-  //           />
-  //         ) : (
-  //           <img
-  //             className="max-w-xs mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
-  //             src={image}
-  //             alt={title}
-  //           />
-  //         )}
+  const { description, image, title, published_at, source, author }: DataEntry =
+    searchParams;
+  return (
+    <section>
+      <article className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
+        {!image ? (
+          <Image
+            className="max-w-xs mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
+            alt="image"
+            src={defaultImg}
+          />
+        ) : (
+          <img
+            className="max-w-xs mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
+            src={image}
+            alt={title}
+          />
+        )}
 
-  //         <div className="px-10">
-  //           <h1 className="headerTitle px-0 pb-2">{title}</h1>
-  //           <div className="flex space-x-4 divide-x-2">
-  //             <h2 className="font-bold">By : {author || "Unknown"}</h2>
-  //             <h2 className="font-bold pl-4">Source : {source || "Unknown"}</h2>
-  //             <p className="pl-4">{published_at}</p>
-  //           </div>
-  //           <p className="pt-4">{description}</p>
-  //         </div>
-  //       </article>
-  //     </section>
-  //   );
-
-  return <div>{Object.entries(searchParams!).length}!!!</div>;
+        <div className="px-10">
+          <h1 className="headerTitle px-0 pb-2">{title}</h1>
+          <div className="flex space-x-4 divide-x-2">
+            <h2 className="font-bold">By : {author || "Unknown"}</h2>
+            <h2 className="font-bold pl-4">Source : {source || "Unknown"}</h2>
+            <p className="pl-4">{published_at}</p>
+          </div>
+          <p className="pt-4">{description}</p>
+        </div>
+      </article>
+    </section>
+  );
 }
 
 export default ArticlePage;
-
-// export async function getServerSideProps(context) {
-//   return {
-//     props: { searchParams }, // will be passed to the page component as props
-//   };
-// }
