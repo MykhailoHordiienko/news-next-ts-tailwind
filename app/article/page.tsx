bd7415a677fe1c71cada5e2abb5efceebb543bda
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-03-12 19:11:17 +0200

    debug

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -7,44 +7,46 @@ type Props = {
 };
 
 const ArticlePage = ({ searchParams }: Props) => {
-  if (
-    (searchParams && Object.entries(searchParams).length) === 0 ||
-    !searchParams
-  ) {
-    return notFound();
-  }
+  //   if (
+  //     (searchParams && Object.entries(searchParams).length) === 0 ||
+  //     !searchParams
+  //   ) {
+  //     return notFound();
+  //   }
 
-  const { description, image, title, published_at, source, author } =
-    searchParams;
-  return (
-    <section>
-      <article className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
-        {!image ? (
-          <Image
-            className="max-w-xs mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
-            alt="image"
-            src={defaultImg}
-          />
-        ) : (
-          <img
-            className="max-w-xs mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
-            src={image}
-            alt={title}
-          />
-        )}
+  //   const { description, image, title, published_at, source, author }: DataEntry =
+  //     searchParams;
+  //   return (
+  //     <section>
+  //       <article className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
+  //         {!image ? (
+  //           <Image
+  //             className="max-w-xs mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
+  //             alt="image"
+  //             src={defaultImg}
+  //           />
+  //         ) : (
+  //           <img
+  //             className="max-w-xs mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
+  //             src={image}
+  //             alt={title}
+  //           />
+  //         )}
 
-        <div className="px-10">
-          <h1 className="headerTitle px-0 pb-2">{title}</h1>
-          <div className="flex space-x-4 divide-x-2">
-            <h2 className="font-bold">By : {author || "Unknown"}</h2>
-            <h2 className="font-bold pl-4">Source : {source || "Unknown"}</h2>
-            <p className="pl-4">{published_at}</p>
-          </div>
-          <p className="pt-4">{description}</p>
-        </div>
-      </article>
-    </section>
-  );
+  //         <div className="px-10">
+  //           <h1 className="headerTitle px-0 pb-2">{title}</h1>
+  //           <div className="flex space-x-4 divide-x-2">
+  //             <h2 className="font-bold">By : {author || "Unknown"}</h2>
+  //             <h2 className="font-bold pl-4">Source : {source || "Unknown"}</h2>
+  //             <p className="pl-4">{published_at}</p>
+  //           </div>
+  //           <p className="pt-4">{description}</p>
+  //         </div>
+  //       </article>
+  //     </section>
+  //   );
+
+  return <div>ARTICLE!!!</div>;
 };
 
 export default ArticlePage;
