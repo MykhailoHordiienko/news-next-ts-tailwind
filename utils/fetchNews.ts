3f5b21f5abec0449ccaf3f9ce21db669539e1d2b
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-03-20 09:48:07 +0200

    fix

--- utils/fetchNews.ts
+++ utils/fetchNews.ts
@@ -5,7 +5,9 @@ const { BASE_URL_PROD = "http://api.mediastack.com/v1/" } = process.env;
 
 export const fetchNews = async (rout?: string, category?: string) => {
   try {
-    const res = await axios.get(`${BASE_URL_PROD}${rout}=${category}`);
+    const res = await axios.get<NewsResponse>(
+      `${BASE_URL_PROD}${rout}=${category}`
+    );
     const data = sortNewsByImg(res.data);
     return data;
   } catch (err) {
